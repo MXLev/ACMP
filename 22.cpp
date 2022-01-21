68521e5dd52e31783d52019eb0bd0392d1031aa2
MXLev <skuratov.lev@gmail.com>
2022-01-21 20:58:46 +0600

    327

---
+++ 22.cpp
@@ -0,0 +1,25 @@
+//
+// https://acmp.ru/index.asp?main=task&id_task=22
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr;
+    while (n / 2 != 0){
+        arr.push_back(n % 2);
+        n /= 2;
+    }
+    int answ = 0;
+    for (int i : arr) {
+        if (i == 1){
+            answ++;
+        }
+    }
+    cout << answ;
+}
--- 327.cpp
+++ 327.cpp
@@ -13,28 +13,46 @@ int main(){
         string answ;
         cin >> answ;
         bool bool1 = false, bool2 = false;
-        for (int j = 0; j < 2; ++j) {
-            int sum1 = 0, sum2 = 0;
-            for (int k = 0; j < 3; ++j) {
-                sum1 += answ[k] - 48;
-            }
-            for (int k = 0; j < 3; ++j) {
-                sum2 += answ[k + 3] - 48;
-            }
-            if (j == 0 && sum1 == sum2){
-                bool1 = true;
-            } else if (sum1 == sum2 && i == 1){
-                bool2 = true;
-            } else {
-                bool1 = false;
-                bool2 = false;
-            }
+        string tempansw1 = to_string(stoi(answ) - 1);
+        while (tempansw1.size() != answ.size()){
+            string temp = "0";
+            temp += tempansw1;
+            tempansw1 = temp;
         }
-        if (bool1 && bool2){
-            cout << "YES" << '\n';
+        int sum1 = 0, sum2 = 0;
+        for (int j = 0; j < 3; ++j) {
+            sum1 += tempansw1[j] - 48;
+        }
+        for (int j = 0; j < 3; ++j) {
+            sum2 += tempansw1[j + 3] - 48;
+        }
+        if (sum1 == sum2){
+            bool1 = true;
         } else {
-            cout << "NO" << '\n';
-
+            bool1 = false;
+        }
+        sum1 = 0, sum2 = 0;
+        string tempansw2 = to_string(stoi(answ) + 1);
+        while (tempansw2.size() != answ.size()){
+            string temp = "0";
+            temp += tempansw2;
+            tempansw2 = temp;
+        }
+        for (int j = 0; j < 3; ++j) {
+            sum1 += tempansw2[j] - 48;
+        }
+        for (int j = 0; j < 3; ++j) {
+            sum2 += tempansw2[j + 3] - 48;
+        }
+        if (sum1 == sum2){
+            bool2 = true;
+        } else {
+            bool2 = false;
+        }
+        if (bool1 || bool2){
+            cout << "Yes" << '\n';
+        } else {
+            cout << "No" << '\n';
         }
     }
 }
