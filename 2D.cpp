ef65c61441a4ffc9a2662e8c29278f9fb0b72344
MXLev <skuratov.lev@gmail.com>
2022-03-28 19:36:02 +0600

    2D 3A

---
+++ 2D.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Beast on 25.03.2022.
+// https://informatics.msk.ru/mod/statements/view.php?id=192&chapterid=111728#1
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<int> arr(n);
+    vector<int> arr2(m);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < m; ++i) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < ; ++i) {
+        
+    }
+}
--- 3A.cpp
+++ 3A.cpp
@@ -6,12 +6,24 @@
 
 using namespace std;
 
-int main() {
-    int w, h, n;
-    cin >> w >> h >> n;
-    int left = 0, right = max(w * n, h * n) - 1;
-    //n <= l / h * l / w
-    while (right - left > 1){
+typedef long long ll;
 
+ll w, h, n;
+
+bool calculate(ll x){
+    return n <= x / h * x / w;
+}
+
+int main() {
+    cin >> w >> h >> n;
+    ll left = 0, right = (ll)1e18 + 100;
+    while (right - left > 1){
+        ll x = (right + left) >> 1;
+        if (calculate(x)){
+            right = x;
+        } else {
+            left = x;
+        }
     }
+    cout << right;
 }
