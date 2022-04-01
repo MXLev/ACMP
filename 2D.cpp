6b6afa487dd0591f20015d40e61df016c3724b7d
MXLev <skuratov.lev@gmail.com>
2022-04-01 20:04:57 +0600

    2D

--- 2D.cpp
+++ 2D.cpp
@@ -7,20 +7,42 @@
 #include <algorithm>
 
 using namespace std;
+typedef long int li;
 
 int main(){
-    int n, m;
+    li n, m;
     cin >> n >> m;
-    vector<int> arr(n);
-    vector<int> arr2(m);
-    for (int i = 0; i < n; ++i) {
+    vector<li> arr(n);
+    vector<li> arr2(m);
+    for (li i = 0; i < n; ++i) {
         cin >> arr[i];
     }
-    for (int i = 0; i < m; ++i) {
-        cin >> arr[i];
+    for (li i = 0; i < m; ++i) {
+        cin >> arr2[i];
     }
-
-    for (int i = 0; i < ; ++i) {
-        
+    li left, right;
+    for (li i = 0; i < m; ++i) {
+        left = -1, right = n;
+        if (arr2[i] > arr[n - 1] || arr2[i] < arr[0]){
+            cout << 0 << '\n';
+            continue;
+        }
+        while (right - left != 1){
+            if (arr[(right + left) / 2] > arr2[i]){
+                right = (right + left) / 2;
+            } else if (arr[(right + left) / 2] <= arr2[i]) {
+                left = (right + left) / 2;
+            }
+        }
+        if (arr[left] != arr2[i]) {
+            cout << 0 << '\n';
+            continue;
+        }
+        li j = left;
+        while (j != n&& arr[j] == arr2[i]){
+            right = j;
+            j--;
+        }
+        cout << right + 1 << ' ' << left + 1 << '\n';
     }
 }
