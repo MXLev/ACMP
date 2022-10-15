a254c641d2b72e8a3f1495ba554e1eb6c7cb4ee7
MXLev <skuratov.lev@gmail.com>
2022-10-15 20:01:37 +0600

    4H

--- 4H.cpp
+++ 4H.cpp
@@ -5,25 +5,56 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+typedef long long ll;
 
 int main(){
-    long long n;
+    ll n;
     cin >> n;
-    vector<pair<long long, long long>> arr;
-    int last = 0;
-    for (long long i = 0; i < n; ++i) {
-        int x, val;
+    vector<pair<ll, ll>> arr;
+    for (ll i = 0; i < n; ++i) {
+        ll x, val;
         cin >> x >> val;
-        arr.emplace_back(x, val + last);
-        last = val;
+        if (val == 0){
+            continue;
+        }
+        arr.emplace_back(x, val);
     }
     sort(arr.begin(), arr.end());
-    long long q;
+    int last = 0;
+    for (int i = 0; i < arr.size(); ++i) {
+        arr[i].second += last;
+        last = arr[i].second;
+    }
+    ll q;
     cin >> q;
     for (int i = 0; i < q; ++i) {
-        int l, r;
+        ll l, r;
+        cin >> l >> r;
 
-        int left = 0, right = arr.end();
-        while (right - left > 1)
+        ll leftL = 0, rightL = arr.size();
+        while (rightL - leftL > 1){
+            if (arr[(leftL + rightL) / 2].first > l) {
+                rightL = (leftL + rightL) / 2;
+            } else if (arr[(leftL + rightL) / 2].first < l){
+                leftL = (leftL + rightL) / 2;
+            } else {
+                break;
+            }
+        }
+
+        ll leftR = 0, rightR = arr.size();
+        while (rightR - leftR > 1){
+            if (arr[(leftL + rightL) / 2].first) {
+                rightR = (leftL + rightL) / 2;
+            } else if (arr[(leftL + rightL) / 2].first){
+                leftR = (leftL + rightL) / 2;
+            } else {
+                break;
+            }
+        }
+        if (rightL != n || rightR != n){
+            cout << arr[leftR].second - arr[leftR].second << '\n';
+        }
+
     }
 }
